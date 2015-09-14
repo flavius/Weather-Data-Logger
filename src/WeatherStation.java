e5f7b7f995ff96559fa9f3b186e9ac4c45828766
Michael Bruce Allen <CodeAmend@Gmail.Com>
2015-09-14 08:01:31 -0400

    added setInitialValues method

--- src/WeatherStation.java
+++ src/WeatherStation.java
@@ -1,49 +1,66 @@
-
 public class WeatherStation {
 
+    WeatherAttribute humidity = new Humidity();
+    WeatherAttribute temperature = new Temperature();
+    WeatherAttribute pressure = new Pressure();
+
+
+    WeatherStation() {
+        setInitialValues();
+    }
+
+    private void setInitialValues() {
+        this.humidity.setData(0);
+        this.temperature.setData(0);
+        this.pressure.setData(0);
+    }
 
 
 }
 
+
+//public class WeatherStation {
 //
 //
-//private int pressure;
-//private int temperature;
-//private int humidity;
-//private List<Observer> observersList;
+//    private int pressure;
+//    private int temperature;
+//    private int humidity;
+//    private List<Observer> observersList;
 //
-//	public WeatherStation(){
-//		this.observersList = new ArrayList<>();
-//	}
+//    public WeatherStation() {
 //
-//	@Override
-//	public void addObserver(Observer o) {
-//		this.observersList.add(o);
-//	}
+//        this.observersList = new ArrayList<>();
+//    }
 //
-//	@Override
-//	public void removeObserver(Observer o) {
-//		this.observersList.remove(observersList.indexOf(o));
-//	}
+//    @Override
+//    public void addObserver(Observer o) {
+//        this.observersList.add(o);
+//    }
 //
-//	@Override
-//	public void notifyAllObservers() {
-//		for(Observer observer : this.observersList){
-//			observer.update(pressure, temperature, humidity);
-//		}
-//	}
+//    @Override
+//    public void removeObserver(Observer o) {
+//        this.observersList.remove(observersList.indexOf(o));
+//    }
 //
-//	public void setPressure(int pressure) {
-//		this.pressure = pressure;
-//		notifyAllObservers();
-//	}
+//    @Override
+//    public void notifyAllObservers() {
+//        for (Observer observer : this.observersList) {
+//            observer.update(pressure, temperature, humidity);
+//        }
+//    }
 //
-//	public void setTemperature(int temperature) {
-//		this.temperature = temperature;
-//		notifyAllObservers();
-//	}
+//    public void setPressure(int pressure) {
+//        this.pressure = pressure;
+//        notifyAllObservers();
+//    }
 //
-//	public void setHumidity(int humidity) {
-//		this.humidity = humidity;
-//		notifyAllObservers();
-//	}
+//    public void setTemperature(int temperature) {
+//        this.temperature = temperature;
+//        notifyAllObservers();
+//    }
+//
+//    public void setHumidity(int humidity) {
+//        this.humidity = humidity;
+//        notifyAllObservers();
+//    }
+//}
