73f7b04cddb903b5f73f120111cfd227a117d7b0
Michael Bruce Allen <CodeAmend@Gmail.Com>
2015-09-15 10:41:04 -0400

    WeatherStation object created and passed

--- src/App.java
+++ src/App.java
@@ -9,14 +9,14 @@ public class App {
         WeatherStation station_2 = new WeatherStation("Florida");
         WeatherStation station_3 = new WeatherStation("Idaho");
 
-        Display display = new ConsoleDisplay();
-
-        display.attatchObserver(station_1);
-        display.attatchObserver(station_2);
-
-        station_1.updateData(new Temperature(67));
-        station_2.updateData(new Humidity(55));
-        station_3.updateData(new Pressure(42));
+//        Display display = new ConsoleDisplay();
+//
+//        display.attatchObserver(station_1);
+//        display.attatchObserver(station_2);
+//
+//        station_1.updateData(new Temperature(67));
+//        station_2.updateData(new Humidity(55));
+//        station_3.updateData(new Pressure(42));
 
         //This should display 2 different lines on the console
         //each station on a different line with data displayed.
---
+++ src/WeatherStation.java
@@ -0,0 +1,12 @@
+/**
+ * Created by codeamend on 9/15/15.
+ */
+public class WeatherStation {
+
+    private String name;
+
+    WeatherStation(String name) {
+        this.name = name;
+    }
+
+}
